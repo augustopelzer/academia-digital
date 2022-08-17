43308fe709ae30a5089b18fc743f76cef1f3552c
Augusto <augustopelzer@gmail.com>
2022-08-17 16:41:41 -0300

    Desafio Academia Digital Dio com a Implementação do CRUD do Aluno

---
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -0,0 +1,49 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Aluno get(@PathVariable Long id) {
+        return service.get(id);
+    }
+
+    @PostMapping("/update/{id}")
+    public Aluno update(@PathVariable Long id,@RequestBody AlunoUpdateForm formUpdate) {
+        return service.update(id, formUpdate);
+    };
+
+    @PostMapping
+    public Aluno create(@RequestBody AlunoForm form) {
+        return service.create(form);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id){
+        return service.getAllAvaliacaoFisicaId(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+}
